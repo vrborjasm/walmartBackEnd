de7a4f374dad7d103aca48703ab7b28dcb211cc4
Vicente Borjas <vrborjasm@gmail.com>
2022-01-16 09:44:40 -0300

    feat(controller.product.js): add fn isPalindrome and refactoring controller

--- src/infrastructure/server/routes/controllers/product.js
+++ src/infrastructure/server/routes/controllers/product.js
@@ -1,19 +1,38 @@
 const Product = require('../../../db/models/Product').Product
 
-const productsRouteController = async ctx => {
-  const {text, limit = 10, page = 1} = ctx.request.query
+const isPalindrome = filter => {
+  return (+filter || filter.length > 3) && filter === filter.split('').reverse().join('')
+}
 
-  const productById = +text ? await Product.findOne({id: +text}) : null
+const productsRouteController = async ctx => {
+  const {text='', limit = 10, page = 1} = ctx.request.query
   const options = {
     page,
     limit,
   }
 
-  const products = productById
-    ? [productById]
-    : await Product.paginate({}, options)
+  const filter = +text
+    ? {id: +text}
+    : text
+    ? {$or: [{brand: {$regex: text}}, {description: {$regex: text}}]}
+    : {}
 
-  return (ctx.body = products)
+  const results = await Product.paginate(filter, options)
+
+  return (ctx.body = {
+    ...results,
+    docs: isPalindrome(text)
+      ? results.docs.map(({_id, id, brand, description, image, price}) => ({
+          _id,
+          id,
+          brand,
+          description,
+          image,
+          price,
+          finalPrice: Math.round(price / 2, 1),
+        }))
+      : results.docs,
+  })
 }
 
 module.exports = {productsRouteController}
