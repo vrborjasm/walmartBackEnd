cba774d292edb9d8d82b808570ae5ef02a0e0986
Vicente Borjas <vrborjasm@gmail.com>
2022-01-16 07:40:04 -0300

    feat(routes.index.js): add products route

--- src/infrastructure/server/routes/index.js
+++ src/infrastructure/server/routes/index.js
@@ -1,7 +1,9 @@
 const {pingRouteController} = require('./controllers/ping')
+const {productsRouteController} = require('./controllers/product')
 
 function loadRoutes(router) {
   router.get('/ping', pingRouteController)
+  router.get('/products', productsRouteController)
 
   return router
 }
